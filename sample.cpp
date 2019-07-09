ed5227bdef8ba2ebfca3b189561052bc8719a5d5
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-09 16:12:54 +0900

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "gromacs/fileio/xtcio.h"
+
+using namespace std;
+int main (int argc, char** argv)
+{
+
+
+        t_fileio* fi = open_xtc(argv[1], "r");
+
+        int n;
+
+        gmx_int64_t step;
+
+        real time;
+        real prec;
+
+        matrix box;
+
+        rvec *x;
+
+        gmx_bool bOK;
+
+        // 原子数もここでゲット
+        read_first_xtc(fi, &n, &step, &time, box, &x, &prec, &bOK);
+        cout << "REMARK natom = " <<  n << '\n';
+        read_next_xtc(fi, n, &step, &time, box, x, &prec, &bOK);
+        cout << "Time: " << time << '\n';
+        cout << "Step: " << step << '\n';
+        cout << "Prec: " << prec << '\n';
+        cout << "boxx: " << box[0][0] << '\n';
+        cout << "boxy: " << box[1][1] << '\n';
+        cout << "boxz: " << box[2][2] << '\n';
+        cout << x[0][0] << '\n';
+        cout << x[0][1] << '\n';
+        cout << x[0][2] << '\n';
+        cout << x[1][0] << '\n';
+        cout << x[1][1] << '\n';
+        cout << x[1][2] << '\n';
+
+        close_xtc(fi);
+}
